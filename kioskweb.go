bac443b0a502c0f6989f7cab70b89577afb07703
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2021-08-26 22:16:27 +0900

    refactor: remove generation code from tools.go to kioskweb.go

--- kioskweb.go
+++ kioskweb.go
@@ -2,6 +2,12 @@
 
 package kioskweb
 
+//go:generate go run golang.org/x/sys/windows/mkwinsyscall -output user32.go kioskweb.go
+
+//sys EnumWindows(lpEnumFunc uintptr, lParam uintptr) (err error) = user32.EnumWindows
+//sys GetWindowTextW(hwnd syscall.Handle, text *uint16, nMaxCount int32) (err error) = user32.GetWindowTextW
+//sys SetForegroundWindow(hwnd syscall.Handle) (err error) = user32.SetForegroundWindow
+
 import (
 	"context"
 	"errors"
